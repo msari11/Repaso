557af5ca50cda9031c81026ffbfeb1f090627854
msari11 <msarahi1996@hotmail.com>
2019-10-05 17:24:16 -0600

    Ejercicio 3 Semana 1

---
+++ Ejercicio3 Semana1/src/ejercicio3/semana1/Ejercicio3Semana1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio3.semana1;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class Ejercicio3Semana1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida por consola. 
+//        Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
+        String nombre = "Sari";
+        System.out.println("Bienvenida "+nombre);
+    }
+    
+}
